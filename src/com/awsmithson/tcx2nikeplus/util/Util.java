66a87cc1ad216da6187cd67dd5c171b7708e707f
angus <angussmithson@hotmail.com>
2011-01-04 20:57:07 +0000

    Removed unused import.

--- src/com/awsmithson/tcx2nikeplus/util/Util.java
+++ src/com/awsmithson/tcx2nikeplus/util/Util.java
@@ -28,7 +28,6 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 
