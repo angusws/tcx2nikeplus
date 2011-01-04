5117b30fb0475308914be38c00685827b69ecb89
angus <angussmithson@hotmail.com>
2011-01-04 20:57:44 +0000

    Tidied up log formatting.

--- src/com/awsmithson/tcx2nikeplus/util/LogFormatter.java
+++ src/com/awsmithson/tcx2nikeplus/util/LogFormatter.java
@@ -1,5 +1,7 @@
 package com.awsmithson.tcx2nikeplus.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
@@ -12,13 +14,12 @@ public class LogFormatter extends Formatter
 
 	@Override
 	public String format(LogRecord record) {
-
 		Throwable t = record.getThrown();
-		if (t != null) t.printStackTrace();
-		
-		return (t == null)
-			? String.format(FORMAT_NORMAL, record.getMillis(), record.getLevel(), record.getMessage())
-			: String.format(FORMAT_EXCEPTION, record.getMillis(), record.getLevel(), record.getMessage(), t.toString())
-		;
+		if (t != null) {
+			StringWriter sw = new StringWriter();
+			t.printStackTrace(new PrintWriter(sw));
+			return String.format(FORMAT_EXCEPTION, record.getMillis(), record.getLevel(), record.getMessage(), sw.toString());
+		}
+		else return String.format(FORMAT_NORMAL, record.getMillis(), record.getLevel(), record.getMessage());
 	}
 }
