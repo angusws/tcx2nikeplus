870e2d8be1a68a7effd2a3fd91b34cfb2f93b6eb
angus@awsmithson.com <angussmithson@gmail.com>
2014-06-10 22:33:57 +0100

    Add new methods to Garmin.java which will use JAXB unmarshallers on responses from Garmin http requests, returning java representations

--- src/main/java/com/awsmithson/tcx2nikeplus/http/Garmin.java
+++ src/main/java/com/awsmithson/tcx2nikeplus/http/Garmin.java
@@ -1,49 +1,46 @@
 package com.awsmithson.tcx2nikeplus.http;
 
 import com.awsmithson.tcx2nikeplus.util.Log;
+import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrainingCenterDatabaseT;
+import com.google.common.base.Preconditions;
+import com.topografix.gpx._1._1.GpxType;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Document;
 
+import javax.annotation.Nonnull;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.w3c.dom.Document;
 
+public class Garmin {
 
-public class Garmin
-{
-	// TODO: Tidy up exception-handling... java 7?
-	private static Properties garminProperties;
+	// Load "garmin.properties" file.
+	private static final @Nonnull Properties garminProperties = new Properties();
 	static {
-		garminProperties = new Properties();
-		InputStream in = null;
-		try {
-			in = Garmin.class.getResourceAsStream("/garmin.properties");
-			garminProperties.load(in);
+
+		try (InputStream inputStream = Garmin.class.getResourceAsStream("/garmin.properties")) {
+			garminProperties.load(inputStream);
 		}
 		catch (IOException ioe) {
-			throw new RuntimeException(ioe);
-		}
-		finally {
-			try {
-				if (in != null) in.close();
-			}
-			catch (IOException ioe) {
-				throw new RuntimeException(ioe);
-			}
+			throw new ExceptionInInitializerError(ioe);
 		}
 	}
 	
@@ -54,50 +51,71 @@ public class Garmin
 
 	private static final Log log = Log.getInstance();
 
-	public Garmin() {
-	}
-
-
 	private static final String GARMIN_ID_ERROR = "Invalid Garmin Activity.  Please ensure your garmin workout is not marked as private.";
 
 
-
-	public static CloseableHttpClient getGarminHttpSession() throws IOException {
-
-		// Setup
+	public static @Nonnull CloseableHttpClient getGarminHttpSession() throws IOException {
 		CloseableHttpClient client = HttpClientBuilder.create().build();
 
 		// 1 - Initial attempt to view activities, which will set cookies and redirect us to sign-in page.
 		HttpGet get = new HttpGet(URL_GARMIN_ACTIVITIES);
-		HttpResponse response = client.execute(get);
-		EntityUtils.consume(response.getEntity());
+		try (CloseableHttpResponse response1 = client.execute(get)) {
+			EntityUtils.consume(response1.getEntity());
 
-		// 2 - Sign-in attempt
-		HttpPost post = new HttpPost(URL_GARMIN_SIGN_IN);
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
-        nameValuePairs.add(new BasicNameValuePair("login", "login"));
-        nameValuePairs.add(new BasicNameValuePair("login:loginUsernameField", garminProperties.getProperty("GARMIN_USERNAME")));
-        nameValuePairs.add(new BasicNameValuePair("login:password", garminProperties.getProperty("GARMIN_PASSWORD")));
-		nameValuePairs.add(new BasicNameValuePair("javax.faces.ViewState", "j_id1"));
-        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-		response = client.execute(post);
-		EntityUtils.consume(response.getEntity());
+			// 2 - Sign-in attempt
+			HttpPost post = new HttpPost(URL_GARMIN_SIGN_IN);
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(4);
+			nameValuePairs.add(new BasicNameValuePair("login", "login"));
+			nameValuePairs.add(new BasicNameValuePair("login:loginUsernameField", garminProperties.getProperty("GARMIN_USERNAME")));
+			nameValuePairs.add(new BasicNameValuePair("login:password", garminProperties.getProperty("GARMIN_PASSWORD")));
+			nameValuePairs.add(new BasicNameValuePair("javax.faces.ViewState", "j_id1"));
+			post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			try (CloseableHttpResponse response2 = client.execute(post)) {
+				EntityUtils.consume(response2.getEntity());
+			}
 
-		return client;
+			return client;
+		}
+	}
+
+
+	public static @Nonnull TrainingCenterDatabaseT downloadTcx(@Nonnull CloseableHttpClient httpClient, int garminActivityId) throws IOException, JAXBException {
+		Preconditions.checkNotNull(httpClient, "httpClient argument was null.");
+
+		HttpGet get = new HttpGet(String.format(URL_GARMIN_TCX, garminActivityId));
+		try (CloseableHttpResponse response = httpClient.execute(get)) {
+			JAXBContext jaxbContext = JAXBContext.newInstance(TrainingCenterDatabaseT.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+			return unmarshaller.unmarshal(new StreamSource(response.getEntity().getContent()), TrainingCenterDatabaseT.class).getValue();
+		}
+	}
+
+
+	public static @Nonnull GpxType downloadGpx(@Nonnull CloseableHttpClient httpClient, int garminActivityId) throws IOException, JAXBException {
+		Preconditions.checkNotNull(httpClient, "httpClient argument was null.");
+
+		HttpGet get = new HttpGet(String.format(URL_GARMIN_GPX, garminActivityId));
+		try (CloseableHttpResponse response = httpClient.execute(get)) {
+			JAXBContext jaxbContext = JAXBContext.newInstance(GpxType.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+			return unmarshaller.unmarshal(new StreamSource(response.getEntity().getContent()), GpxType.class).getValue();
+		}
 	}
 
 
 
 
-	public static Document downloadGarminTcx(HttpClient client, int garminActivityId) throws Throwable {
+
+
+	public static Document downloadGarminTcx(HttpClient client, int garminActivityId) throws IOException {
 		return downloadGarminDocument(client, URL_GARMIN_TCX, garminActivityId);
 	}
-	public static Document downloadGarminGpx(HttpClient client, int garminActivityId) throws Throwable {
+	public static Document downloadGarminGpx(HttpClient client, int garminActivityId) throws IOException {
 		return downloadGarminDocument(client, URL_GARMIN_GPX, garminActivityId);
 	}
 
 
-	private static Document downloadGarminDocument(HttpClient client, String url, int garminActivityId) throws Throwable {
+	private static Document downloadGarminDocument(HttpClient client, String url, int garminActivityId) throws IOException {
 		url = String.format(url, garminActivityId);
 		Document doc = null;
 
@@ -108,60 +126,12 @@ public class Garmin
 		}
 		catch (Exception e) {
 			log.out(e);
-			throw new Exception(GARMIN_ID_ERROR);
+			throw new IOException(GARMIN_ID_ERROR);
 		}
 
-		if (doc == null) throw new Exception(GARMIN_ID_ERROR);
+		if (doc == null) throw new IOException(GARMIN_ID_ERROR);
 
 		log.out("Successfully downloaded data for garmin activity %d.", garminActivityId);
 		return doc;
 	}
-
-	
-
-
-	public static void main(String[] args) {
-
-		HttpClient client = null;
-		try {
-			client = getGarminHttpSession();
-			downloadGarminTcx(client, 83638060);
-			downloadGarminGpx(client, 83638060);
-			client.getConnectionManager().shutdown();
-		}
-		catch (Throwable t) {
-			t.printStackTrace();
-		}
-		finally {
-			if (client != null) client.getConnectionManager().shutdown();
-		}
-		
-	}
 }
-
-/*
-private void syncDataNonGps(String pin, File file) throws MalformedURLException, IOException, ParserConfigurationException, SAXException {
-	// Load the file, ensuring it is valid xml
-	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	DocumentBuilder db = dbf.newDocumentBuilder();
-	Document doc = db.parse(file);
-	doc.normalize();
-
-	syncDataNonGps(pin, doc);
-}
-
-
-private void syncDataGps(String pin, File runXml, File gpxXml) throws MalformedURLException, IOException, ParserConfigurationException, SAXException, NoSuchAlgorithmException, KeyManagementException {
-	// Load the file, ensuring it is valid xml
-	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-	DocumentBuilder db = dbf.newDocumentBuilder();
-
-	Document runXmlDoc = db.parse(runXml);
-	runXmlDoc.normalize();
-
-	Document gpxXmlDoc = db.parse(gpxXml);
-	runXmlDoc.normalize();
-
-	syncDataGps(pin, runXmlDoc, gpxXmlDoc);
-}
-*/
