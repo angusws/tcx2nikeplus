5ee993b00349bd1d3e5d22a16057a9abdf213985
angus@awsmithson.com <angussmithson@gmail.com>
2014-10-05 14:26:28 +0100

    Tidy up JAXBObject.java
    1) Consolidate duplicated code.
    2) Allow multiple classes to be bound on our JAXBContext objects).

--- src/main/java/com/awsmithson/tcx2nikeplus/jaxb/JAXBObject.java
+++ src/main/java/com/awsmithson/tcx2nikeplus/jaxb/JAXBObject.java
@@ -1,5 +1,6 @@
 package com.awsmithson.tcx2nikeplus.jaxb;
 
+import com.garmin.xmlschemas.trackpointextension.v1.TrackPointExtensionT;
 import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrainingCenterDatabaseT;
 import com.google.common.base.Preconditions;
 import com.topografix.gpx._1._1.GpxType;
@@ -13,107 +14,65 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.InputStream;
 
 public enum JAXBObject {
-
-	TRAINING_CENTER_DATABASE {
-		private final JAXBContext JAXB_CONTEXT = createTrainingCenterDatabaseJAXBContect();
-		private @Nonnull JAXBContext createTrainingCenterDatabaseJAXBContect() {
-			try {
-				return JAXBContext.newInstance(TrainingCenterDatabaseT.class);
-			} catch (JAXBException je) {
-				throw new ExceptionInInitializerError(je);
-			}
-		}
-		private final @Nonnull ThreadLocal<Unmarshaller> UNMARSHALLER = new ThreadLocal<Unmarshaller>() {
-			protected synchronized Unmarshaller initialValue() {
-				try {
-					return JAXB_CONTEXT.createUnmarshaller();
-				} catch (JAXBException e) {
-					throw new ExceptionInInitializerError(e);
-				}
-			}
-		};
-		private final @Nonnull ThreadLocal<Marshaller> MARSHALLER = new ThreadLocal<Marshaller>() {
-			protected synchronized Marshaller initialValue() {
-				try {
-					Marshaller marshaller = JAXB_CONTEXT.createMarshaller();
-					marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-					return marshaller;
-				} catch (JAXBException e) {
-					throw new ExceptionInInitializerError(e);
-				}
-			}
-		};
-
+	GPX_TYPE {
 		@Override
-		@Nonnull Unmarshaller getUnmarshaller() {
-			return UNMARSHALLER.get();
-		}
-
-		@Override
-		@Nonnull Class getUnmarshalledClass() {
-			return TrainingCenterDatabaseT.class;
-		}
-
-		@Override
-		public @Nonnull Marshaller getMarshaller() {
-			return MARSHALLER.get();
+		@Nonnull Class[] getClassesToBeBound() {
+			return new Class[] { GpxType.class, TrackPointExtensionT.class };
 		}
 	},
 
-	GPX_TYPE {
-		private final JAXBContext JAXB_CONTEXT = createGpxTypeJAXBContect();
-		private @Nonnull JAXBContext createGpxTypeJAXBContect() {
-			try {
-				return JAXBContext.newInstance(GpxType.class);
-			} catch (JAXBException je) {
-				throw new ExceptionInInitializerError(je);
-			}
-		}
-		private final @Nonnull ThreadLocal<Unmarshaller> UNMARSHALLER = new ThreadLocal<Unmarshaller>() {
-			protected synchronized Unmarshaller initialValue() {
-				try {
-					return JAXB_CONTEXT.createUnmarshaller();
-				} catch (JAXBException e) {
-					throw new ExceptionInInitializerError(e);
-				}
-			}
-		};
-		private final @Nonnull ThreadLocal<Marshaller> MARSHALLER = new ThreadLocal<Marshaller>() {
-			protected synchronized Marshaller initialValue() {
-				try {
-					Marshaller marshaller = JAXB_CONTEXT.createMarshaller();
-					marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-					return marshaller;
-				} catch (JAXBException e) {
-					throw new ExceptionInInitializerError(e);
-				}
-			}
-		};
-
+	TRAINING_CENTER_DATABASE {
 		@Override
-		@Nonnull Unmarshaller getUnmarshaller() {
-			return UNMARSHALLER.get();
-		}
-
-		@Override
-		@Nonnull Class getUnmarshalledClass() {
-			return GpxType.class;
-		}
-
-		@Override
-		public @Nonnull Marshaller getMarshaller() {
-			return MARSHALLER.get();
+		@Nonnull Class[] getClassesToBeBound() {
+			return new Class[] { TrainingCenterDatabaseT.class };
 		}
 	};
 
-	abstract @Nonnull Unmarshaller getUnmarshaller();
-	abstract @Nonnull Class getUnmarshalledClass();
-	public abstract @Nonnull Marshaller getMarshaller();
+	abstract @Nonnull Class[] getClassesToBeBound();
+
+	private final JAXBContext JAXB_CONTEXT = createJAXBContect();
+	private @Nonnull JAXBContext createJAXBContect() {
+		try {
+			return JAXBContext.newInstance(getClassesToBeBound());
+		} catch (JAXBException je) {
+			throw new ExceptionInInitializerError(je);
+		}
+	}
+
+	private final @Nonnull ThreadLocal<Marshaller> MARSHALLER = new ThreadLocal<Marshaller>() {
+		protected synchronized Marshaller initialValue() {
+			try {
+				Marshaller marshaller = JAXB_CONTEXT.createMarshaller();
+				marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+				return marshaller;
+			} catch (JAXBException e) {
+				throw new ExceptionInInitializerError(e);
+			}
+		}
+	};
+
+	private final @Nonnull ThreadLocal<Unmarshaller> UNMARSHALLER = new ThreadLocal<Unmarshaller>() {
+		protected synchronized Unmarshaller initialValue() {
+			try {
+				return JAXB_CONTEXT.createUnmarshaller();
+			} catch (JAXBException e) {
+				throw new ExceptionInInitializerError(e);
+			}
+		}
+	};
+
+	public @Nonnull Marshaller getMarshaller() {
+		return MARSHALLER.get();
+	}
+
+	@Nonnull Unmarshaller getUnmarshaller() {
+		return UNMARSHALLER.get();
+	}
 
 	public @Nonnull <T> T unmarshall(@Nonnull InputStream inputStream) throws JAXBException {
 		Preconditions.checkNotNull(inputStream, "inputStream argument is null.");
 
 		//noinspection unchecked
-		return (T) getUnmarshaller().unmarshal(new StreamSource(inputStream), getUnmarshalledClass()).getValue();
+		return (T) getUnmarshaller().unmarshal(new StreamSource(inputStream), getClassesToBeBound()[0]).getValue();
 	}
 }
