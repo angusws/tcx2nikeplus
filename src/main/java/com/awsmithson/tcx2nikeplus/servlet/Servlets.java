a8a6f06e999f6b36c495765681ea40efd9309a18
angus@awsmithson.com <angussmithson@gmail.com>
2014-10-31 20:17:14 +0000

    Modified how we extract garmin activity-id from a String and added unit-tests for it.
    We now use regex to get the final set of digits.

--- src/main/java/com/awsmithson/tcx2nikeplus/servlet/Servlets.java
+++ src/main/java/com/awsmithson/tcx2nikeplus/servlet/Servlets.java
@@ -4,8 +4,13 @@ import com.google.common.base.Preconditions;
 
 import javax.annotation.Nonnull;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Servlets {
+
+	private static final @Nonnull Pattern DIGIT_MATCHER = Pattern.compile("([0-9]+)");
 
 	static @Nonnull String getRequiredParameter(@Nonnull HttpServletRequest request, @Nonnull String parameterKey) {
 		Preconditions.checkNotNull(request, "request argument is null.");
@@ -25,11 +30,31 @@ public class Servlets {
 	 * Split the string to obtain the activity-id in case the user
 	 * enters the full url "http://connect.garmin.com/activity/23512599"
 	 * instead of just the activityid "23512599".
-	 * @param input
-	 * @return
+	 * Extract the last integer we can find in the input String.  For eaxmple, all of these return {@code}34448379{@code}:
+	 * <ul>
+	 *     <li>{@code}34448379#{@code}</li>
+	 *     <li>{@code}https://connect.garmin.com/activity/34448379{@code}</li>
+	 *     <li>{@code}https://connect.garmin.com/activity/34448379?foo=bar{@code}</li>
+	 *     <li>{@code}https://10.11.12.13/activity/34448379?foo=bar{@code}</li>
+	 * </ul>
+	 * @param input input to extract ID from.
+	 * @return garmin activity-id.
 	 */
-	static int getGarminActivityId(@Nonnull String input) {
+	static int getGarminActivityId(@Nonnull String input) throws IOException {
 		Preconditions.checkNotNull(input, "input argument is null.");
-		return Integer.parseInt(input.substring(input.lastIndexOf('/') + 1));
+
+		// If it ends in hash, take that off - or maybe just grab the last set of numbers?
+		Matcher matcher = DIGIT_MATCHER.matcher(input);
+		if (matcher.find()) {
+			matcher.reset();
+			String lastDigits = "";
+			while (matcher.find()) {
+				lastDigits = matcher.group();
+			}
+			return Integer.parseInt(lastDigits);
+		} else {
+			throw new IOException(String.format("Unable to obtain garmin activity-id from '%s'", input));
+		}
+
 	}
 }
---
+++ src/test/java/com/awsmithson/tcx2nikeplus/servlet/ServletsFastTest.java
@@ -0,0 +1,28 @@
+package com.awsmithson.tcx2nikeplus.servlet;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+public class ServletsFastTest {
+
+	@Test
+	public void testGetGarminActivityId() throws IOException {
+		testGetGarminActivityId(123, "123");
+		testGetGarminActivityId(123, "a/b/c/123");
+		testGetGarminActivityId(123, "ab/c//123");
+		testGetGarminActivityId(123, "123/456/789/123");
+		testGetGarminActivityId(34448379, "34448379");
+		testGetGarminActivityId(34448379, "34448379#");
+		testGetGarminActivityId(34448379, "https://connect.garmin.com/activity/34448379");
+		testGetGarminActivityId(34448379, "https://connect.garmin.com/activity/34448379#");
+		testGetGarminActivityId(34448379, "https://connect.garmin.com/activity/34448379?foo=bar");
+		testGetGarminActivityId(34448379, "https://10.11.12.13/activity/34448379?foo=bar");
+	}
+
+	private void testGetGarminActivityId(int expected, @Nonnull String input) throws IOException {
+		Assert.assertEquals(expected, Servlets.getGarminActivityId(input));
+	}
+}
